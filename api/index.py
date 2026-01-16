41ca37acea89a8cd051b37841f0ef6360f968b40
Juan Pablo Torres <jptorresdota@gmail.com>
2026-01-16 14:12:52 -0500

    Fix Vercel WSGI handler configuration

--- api/index.py
+++ api/index.py
@@ -1,8 +1,5 @@
 import os
 import sys
-import django
-from django.conf import settings
-from django.core.wsgi import get_wsgi_application
 
 # Add the project directory to the Python path
 sys.path.insert(0, '/var/task')
@@ -10,12 +7,6 @@ sys.path.insert(0, '/var/task')
 # Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colibrisalud.settings')
 
-# Setup Django
-django.setup()
+from django.core.wsgi import get_wsgi_application
 
-# Get the WSGI application
 app = get_wsgi_application()
-
-# Vercel serverless handler
-def handler(request):
-    return app(request)
